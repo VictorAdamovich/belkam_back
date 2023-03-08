aa3805eaedcbdb89929a823b943c25738c0d9e83
Victor Adamovich <v.adamovich@pixelplex.io>
2023-03-08 19:28:04 +0300

    first commit

---
+++ src/api/work-plan/controllers/work-plan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-plan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::work-plan.work-plan');
---
+++ src/api/work-plan/routes/work-plan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * work-plan router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::work-plan.work-plan');
